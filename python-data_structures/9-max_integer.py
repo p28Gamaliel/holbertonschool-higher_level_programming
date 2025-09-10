9180f176b6d25978a939b4dc53692aee5fc19a86
p28Gamaliel <gamaliel13perez@gmail.com>
2025-09-10 15:51:49 +0000

    max_integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if my_list == 0:
         return None
-    max_actual = my_list[0]
+    max_value = my_list[0]
     for numero in my_list[1:]:
-        if numero > max_actual:
-            max_actual = numero
-    return max_actual
+        if numero > max_value:
+            max_value = numero
+    return max_value
