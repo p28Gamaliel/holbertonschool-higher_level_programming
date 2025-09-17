8dc80529b5ed18e86386f747be2a15d3376b78a4
p28Gamaliel <gamaliel13perez@gmail.com>
2025-09-17 16:42:36 +0000

    square 2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,13 +3,16 @@
 
 
 class Square:
+
     """asuca"""
 
     def __init__(self, size = 0):
+
         """
         Initializes a new Square instance.
 
         Args:
             size: The size of the square.
         """
+
         self.__size = size
