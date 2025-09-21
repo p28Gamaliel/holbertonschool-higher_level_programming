eaf1fad3905e8fe360a5a208a00b9a56e7132456
p28Gamaliel <gamaliel13perez@gmail.com>
2025-09-21 00:20:51 +0000

    rectangle 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -88,7 +88,7 @@ class Rectangle:
 
     def __str__(self):
         """
-        Returns a string representation of the rectangle using the character '#'.
+        Returns a string representation of the rectangle.
 
         Returns:
             str: A string representing the rectangle, or an empty string if
@@ -96,7 +96,7 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+
         rectangle_str = ""
         for _ in range(self.__height):
             rectangle_str += "#" * self.__width + "\n"
