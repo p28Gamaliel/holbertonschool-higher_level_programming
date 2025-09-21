27678e26bc64655dec263e64593829469e6729ca
p28Gamaliel <gamaliel13perez@gmail.com>
2025-09-21 00:16:19 +0000

    rectangle 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -80,7 +80,7 @@ class Rectangle:
         Calculates and returns the perimeter of the rectangle.
 
         Returns:
-            int: The perimeter of the rectangle. Returns 0 if width or height is 0.
+        The perimeter of the rectangle. Returns 0 if width or height is 0.
         """
         if self.__width == 0 or self.__height == 0:
             return 0
