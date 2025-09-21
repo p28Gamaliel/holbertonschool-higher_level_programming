ea986a94a73e305bcf9c8c27faa37fb731f43f66
p28Gamaliel <gamaliel13perez@gmail.com>
2025-09-21 00:29:25 +0000

    rectangle 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -96,8 +96,7 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-
-            rectangle_str = ""
+        rectangle_str = ""
         for _ in range(self.__height):
             rectangle_str += "#" * self.__width + "\n"
         return rectangle_str.strip()
