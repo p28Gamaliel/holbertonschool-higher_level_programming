097728a019159c594b9b29b9201f83f370a4eeb0
p28Gamaliel <gamaliel13perez@gmail.com>
2025-10-05 19:55:36 +0000

    pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ Pascal's Triangle module """
 
+
 def pascal_triangle(n):
-    """
-    Returns a list of lists of integers representing
-    the Pascal’s triangle of n.
-    """
+    """returns a list of lists of integers representing
+    the Pascal’s triangle of n."""
     if n <= 0:
         return []
 
