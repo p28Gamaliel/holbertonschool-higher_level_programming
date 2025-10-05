1bd65fdcda80c89f80d461d78c52c0426205cc75
p28Gamaliel <gamaliel13perez@gmail.com>
2025-10-05 19:50:57 +0000

    student

---
+++ python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Student class with simplified JSON representation."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Set up the student with name and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a dictionary of attributes."""
+        if isinstance(attrs, list):
+            result = {}
+            for name in attrs:
+                if isinstance(name, str) and hasattr(self, name):
+                    result[name] = getattr(self, name)
+            return result
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance
+        using the values from a dictionary (json)."""
+        for key, value in json.items():
+            setattr(self, key, value)
