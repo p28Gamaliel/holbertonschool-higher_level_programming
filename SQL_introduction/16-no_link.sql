1cf219047f388cba42cb024bd2a950a4d7d46669
p28Gamaliel <gamaliel13perez@gmail.com>
2025-10-30 17:39:47 +0000

    say my name

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,8 +1,4 @@
 -- lists all records of the second_table
-INSERT INTO second_table (score, name) VALUES
-(18, 'Aria'),
-(12, 'Aria');
-
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL AND name != ''
