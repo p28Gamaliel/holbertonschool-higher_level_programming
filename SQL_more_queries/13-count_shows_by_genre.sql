cfe599f2e0bc1ca07dfad1dd259d010a50357dc9
p28gamaliel <gamaliel13perez@gmail.com>
2025-10-31 20:37:49 +0000

    count shows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,10 @@
 -- lists all genres
 SELECT
     g.name AS genre,
-    COUNT(tg.show_id) AS number_of_shows
+    COUNT(sg.show_id) AS number_of_shows
 FROM
-    tv_show_genres tg
-    INNER JOIN genres g ON tg.genre_id = g.id
+    tv_show_genres AS sg
+    JOIN tv_genres AS g ON sg.genre_id = g.id
 GROUP BY
     g.name
 ORDER BY
