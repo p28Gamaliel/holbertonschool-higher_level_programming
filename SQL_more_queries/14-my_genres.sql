70bab47f71fe337d05fca98c596b93d8fc76af64
p28gamaliel <gamaliel13perez@gmail.com>
2025-10-31 20:40:26 +0000

    lists all genres

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- Lists all genres of the show Dexter
+SELECT g.name
+FROM tv_genres AS g
+JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+JOIN tv_shows AS s ON s.id = sg.show_id
+WHERE s.title = 'Dexter'
+ORDER BY g.name ASC;
