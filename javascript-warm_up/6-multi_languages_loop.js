0ecdb13ab672edb5e0eca754a566ab64b90613a7
p28Gamaliel <gamaliel13perez@gmail.com>
2025-11-09 23:25:12 +0000

    loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-const lines = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
-  ];
-  
-  let output = '';
-  
-  for (let i = 0; i < lines.length; i++) {
-    output += lines[i] + (i < lines.length - 1 ? '\n' : '');
-  }
-  
-  console.log(output);
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
