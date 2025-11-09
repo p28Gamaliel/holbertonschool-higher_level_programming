f4a5a1c59831a56a943e12242274283f6ee529db
p28Gamaliel <gamaliel13perez@gmail.com>
2025-11-09 23:54:24 +0000

    add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 function add (a, b) {
-    return a + b;
+  return a + b;
 }
-
 const a = parseInt(process.argv[2], 10);
 const b = parseInt(process.argv[3], 10);
-
 console.log(add(a, b));
+
