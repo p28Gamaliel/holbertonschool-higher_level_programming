48435e805c60dd049ac9da0116db63e09c94ff63
p28Gamaliel <gamaliel13perez@gmail.com>
2025-11-09 23:28:29 +0000

    mult C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const num = parseInt(process.argv[2], 10);
 
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 }
