b397583bec42da830015d55de4b44c8bcb4da800
p28Gamaliel <gamaliel13perez@gmail.com>
2025-11-10 00:00:28 +0000

    factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,8 +4,7 @@ function factorial (n) {
       return 1;
     }
     return n * factorial(n - 1);
-  }
+}
   
-  const n = parseInt(process.argv[2], 10);
-  console.log(factorial(n));
-  
+const n = parseInt(process.argv[2], 10);
+console.log(factorial(n));
