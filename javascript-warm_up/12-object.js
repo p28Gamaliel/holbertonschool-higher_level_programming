03ad4425a43816c312c86233232c43ca793919a4
p28Gamaliel <gamaliel13perez@gmail.com>
2025-11-10 00:10:18 +0000

    object

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -2,7 +2,10 @@
 const myObject = {
     type: 'object',
     value: 12
-};
-console.log(myObject);
-myObject.value = 89;
-console.log(myObject);
+  };
+  console.log(myObject);
+  /*
+  YOUR CODE HERE
+  */
+  myObject.value = 89;
+  console.log(myObject);
