39b552189e88bd816cd464c5182e1ecb15628b3f
p28gamaliel <gamaliel13perez@gmail.com>
2025-11-12 10:21:31 -0300

    change color to red

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,1 +1,7 @@
 #!/bin/usr/node
+const headerElement = document.queryselector('h1');
+if (headerElement) {
+    headerElement.Style.color = '#FF0000';
+} else {
+    console.log("header element not found.");
+}
