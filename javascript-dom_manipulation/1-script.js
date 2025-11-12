8de0b9aa4031fb60fdb2b014310389e7f7266732
p28gamaliel <gamaliel13perez@gmail.com>
2025-11-12 14:45:23 -0300

    click un the button from change color

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,1 +1,6 @@
 #!/bin/usr/node
+const redHeader = document.querySelector('#red_header');
+redHeader.addEventListener('click', () => {
+    const header = document.querySelector('header');
+    header.text.color = '#FF0000';
+});
