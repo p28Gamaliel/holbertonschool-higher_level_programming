9b72816540893389f69fdfe3d1b6a34d1db6c0c3
p28Gamaliel <gamaliel13perez@gmail.com>
2025-12-18 12:56:23 -0300

    fix 0-add_integer style

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,44 +1,9 @@
 #!/usr/bin/python3
-"""
-Module that provides a function to add two integers.
-"""
+"""Module that provides a function to add two integers."""
+
 
 def add_integer(a, b=98):
-    """
-    Adds two integers or floats casted to integers.
-
-    Args:
-        a (int or float): first number.
-        b (int or float): second number, defaults to 98.
-
-    Raises:
-        TypeError: If a or b is not an int or float.
-
-    Returns:
-        int: Sum of a and b.
-
-    Examples:
-    >>> add_integer(1, 2)
-    3
-    >>> add_integer(100, -2)
-    98
-    >>> add_integer(2)
-    100
-    >>> add_integer(100.3, -2)
-    98
-    >>> add_integer(4, "School")
-    Traceback (most recent call last):
-    TypeError: b must be an integer
-    >>> add_integer(None)
-    Traceback (most recent call last):
-    TypeError: a must be an integer
-    >>> add_integer(3.7, 2.3)
-    5
-    >>> add_integer(-1, -2)
-    -3
-    >>> add_integer(0, 0)
-    0
-    """
+    """Adds two integers or floats casted to integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
